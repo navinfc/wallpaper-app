b0d5f528836c65f833922a8fcc0676c6dfdcd21b
Navin Chandrakar <88479575+navinfc@users.noreply.github.com>
2024-10-18 14:47:32 +0530

    added bg image

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, StyleSheet, StatusBar, Image } from "react-native";
+import React from "react";
+import { hp, wp } from "../helpers/common";
 
 const WelcomeScreen = () => {
   return (
-    <View>
-      <Text>WelcomeScreen</Text>
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={require("../assets/images/welcome.png")}
+        style={styles.bgImage}
+        resizeMode="cover"
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  bgImage: {
+    width: wp(100),
+    height: hp(100),
+    position: "absolute",
+  },
+});
 
 export default WelcomeScreen;
