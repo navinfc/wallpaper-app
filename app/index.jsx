3d6f834ab83e45b61eff24676e5571d93219f48e
Navin Chandrakar <88479575+navinfc@users.noreply.github.com>
2024-10-23 13:30:14 +0530

    build home screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,9 @@
-import { View, Text, StyleSheet, StatusBar, Image } from "react-native";
+import { View, Text, StyleSheet, StatusBar, Image, Pressable } from "react-native";
 import React from "react";
 import { hp, wp } from "../helpers/common";
+import { LinearGradient } from "expo-linear-gradient";
+import  Animated, {FadeInDown} from "react-native-reanimated";
+import { theme } from "../constants/theme";
 
 const WelcomeScreen = () => {
   return (
@@ -11,6 +14,28 @@ const WelcomeScreen = () => {
         style={styles.bgImage}
         resizeMode="cover"
       />
+      {/* linear gradient */}
+      <Animated.View entering={FadeInDown.duration(600)} style={{ flex: 1 }}>
+        <LinearGradient colors={['rgba(255, 255, 255, 0)', 'rgba(255, 255, 255, 0.5)', 'white', 'white']}
+        style={styles.gradient}
+        start={{x:0.5, y: 0}}
+        end={{x:0.5, y:0.8}}
+        />
+        {/* Content */}
+        <View style={styles.contentContainer}>
+          <Animated.Text entering={FadeInDown.delay(400).springify()} style={styles.title}>
+            Pixels
+          </Animated.Text>
+          <Animated.Text entering={FadeInDown.delay(500).springify()} style={styles.punchline}>
+            Every Pixels Tells a Story
+          </Animated.Text>
+          <Animated.View entering={FadeInDown.delay(600).springify()}>
+            <Pressable style={styles.startButton}>
+              <Text style={styles.startText}>Start Explore</Text>
+            </Pressable>
+          </Animated.View>
+        </View>
+      </Animated.View>
     </View>
   );
 };
@@ -23,6 +48,43 @@ const styles = StyleSheet.create({
     width: wp(100),
     height: hp(100),
     position: "absolute",
+  },
+  gradient: {
+    width: wp(100),
+    height: hp(65),
+    bottom: 0,
+    position: 'absolute',
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: "flex-end",
+    gap: 14,
+  },
+  title: {
+    fontSize: hp(7),
+    color: theme.colors.neutral(0.9),
+    fontWeight: theme.fontWeights.bold,
+  },
+  punchline: {
+    fontSize: hp(2),
+    letterSpacing: 1,
+    marginBottom: 10,
+    fontWeight: theme.fontWeights.medium,
+  },
+  startButton: {
+    marginBottom: 50,
+    backgroundColor: theme.colors.neutral(0.9),
+    padding: 15,
+    paddingHorizontal: 90,
+    borderRadius: theme.radius.xl,
+    borderCurve: 'continuous',
+  },
+  startText: {
+    color: "white",
+    fontSize: hp(3),
+    fontWeight: theme.fontWeights.medium,
+    letterSpacing: 1
   },
 });
 
